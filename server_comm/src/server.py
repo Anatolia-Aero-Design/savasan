0b683f1bed70937fcabdd08a0727ce1a4fd9508e
Poyrazzo <128072566+Poyrazzo@users.noreply.github.com>
2024-08-24 14:34:45 +0300

    Add files via upload

---
+++ server_comm/src/server.py
@@ -0,0 +1,85 @@
+# server.py
+from flask import Flask, request, jsonify
+from Competition import Competition
+import logging
+
+app = Flask(__name__)
+
+@app.route('/api/telemetri_gonder', methods=['POST'])
+def update_data():
+    json_data = request.get_json()
+    competition.update_contestant(json_data['takim_numarasi'], json_data)
+    response = competition.response_json()
+
+    # Print the received data
+    #print(f"Received data: {json_data}")
+    return jsonify(response), 200
+
+@app.route('/api/kilitlenme_bilgisi', methods=['POST'])
+def kenetlenme_bilgisi_gonder():
+    json_data = request.get_json()
+    #print(f"Received lock-on data: {json_data}")
+    competition.update_lock_on(json_data)
+    response = competition.response_json()
+
+    return jsonify({"status": "success"}), 200
+
+@app.route('/api/sunucusaati', methods=['GET'])
+def get_server_time():
+    current_time = competition.get_current_time()  # Add a method to get only the current time
+    return jsonify({"sunucusaati": current_time}), 200
+
+@app.route('/api/hss_koordinatlari', methods=['GET'])
+def hss_coordinates():
+    hss_koordinatlari = {
+        "sunucusaati": competition.get_current_time(),
+        "hss_koordinat_bilgileri": [
+            {
+                "id": 0,
+                "hssEnlem": 36.939879,
+                "hssBoylam": 35.532268,
+                "hssYaricap": 50
+            },
+            {
+                "id": 1,
+                "hssEnlem": 36.940047,
+                "hssBoylam": 35.537481,
+                "hssYaricap": 50
+            },
+            {
+                "id": 2,
+                "hssEnlem": 36.936330,
+                "hssBoylam": 35.531538,
+                "hssYaricap": 75
+            },
+            {
+                "id": 3,
+                "hssEnlem": 36.937116,
+                "hssBoylam": 35.537972,
+                "hssYaricap": 150
+            }
+        ]
+    }
+    return jsonify(hss_koordinatlari), 200
+
+@app.route('/api/qr_koordinati', methods=['GET'])
+def get_qr_coordinates():
+    qr_latitude = 36.93824690
+    qr_longtitude = 35.52944677
+    qr_coordinates = {
+        "qrEnlem": qr_latitude,
+        "qrBoylam": qr_longtitude
+    }
+    return jsonify(qr_coordinates), 200
+
+@app.route('/api/kamikaze_bilgisi', methods=['POST'])
+def get_qr_data():
+    json_data = request.get_json()
+    print(f"qr data: {json_data}")
+    return jsonify({"qr data": json_data}), 200
+
+
+
+if __name__ == '__main__':
+    competition = Competition()
+    app.run(debug=True, host='127.0.0.1', port=5000)
---
+++ server_comm/src/server_comm/comm_node.py
@@ -0,0 +1,381 @@
+#!/usr/bin/env python
+
+
+import rospy
+from flask import session
+from sensor_msgs.msg import Imu, BatteryState, NavSatFix,TimeReference
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64, Bool, String
+from std_srvs.srv import Empty
+from geometry_msgs.msg import TwistStamped
+from mavros_msgs.msg import State
+import requests
+import json
+import logging
+from server_comm.msg import KonumBilgileri, KonumBilgisi
+from utils import quaternion_to_euler, calculate_speed, unix_to_utc_formatted
+from datetime import datetime
+import time
+import threading
+from image_processor.msg import Yolo_xywh
+
+class Comm_Node:
+    def __init__(self):
+        self.session = requests.Session()
+        self.base_url = 'http://savasaniha.baykartech.com/api'
+        self.username = 'estuanatolia'
+        self.password = '2Eqtm3v3ZJ'
+        self.team_number = None
+       
+       
+        # Get server APIs from sim.launch params
+        self.server_url_telemetri_gonder = rospy.get_param('/comm_node/api/telemetri_gonder')
+        self.server_url_kilitlenme_bilgisi = rospy.get_param('/comm_node/api/kilitlenme_bilgisi')
+        self.server_url_sunucusaati = rospy.get_param('/comm_node/api/sunucusaati')
+        self.server_url_kamikaze_bilgisi = rospy.get_param('/comm_node/api/kamikaze_bilgisi')
+        
+        self.abort_service = rospy.Service('stop_kamikaze', Empty, self.qr_check)
+
+        self.imu = None
+        self.battery = None
+        self.rel_alt = None
+        self.position = None
+        self.speed = None
+        self.state = None
+        self.lock_on = None
+        self.kilit = None
+        self.qr_data = None
+        self.qr_published = False
+        self.fcu_time = None
+        self.fcu_time_nsecs = None 
+        
+        self.bbox = None
+        self.bbox_x = None
+        self.bbox_y = None
+        self.bbox_w = None
+        self.bbox_h = None
+        
+        self.start_time = None
+        self.end_time = None
+        self.kilit_prev = None
+
+        try:
+            # Initialize Subscribers
+            self.imu_sub = rospy.Subscriber('/mavros/imu/data', Imu, self.imu_callback)
+            self.battery_sub = rospy.Subscriber('/mavros/battery', BatteryState, self.battery_callback)
+            self.rel_altitude_sub = rospy.Subscriber('/mavros/global_position/rel_alt', Float64, self.rel_altitude_callback)
+            self.position_sub = rospy.Subscriber('/mavros/global_position/global', NavSatFix, self.position_callback)
+            self.speed_sub = rospy.Subscriber('/mavros/local_position/velocity_local', TwistStamped, self.speed_callback)
+            self.state_sub = rospy.Subscriber('/mavros/state', State, self.state_callback)
+            self.lock_on_sub = rospy.Subscriber('/lock_on_status', Bool, self.lock_on_callback)
+            self.kilit_sub = rospy.Subscriber('/kilit', Bool, self.kilit_callback)
+            self.bbox_sub = rospy.Subscriber("/yolov8/xywh", Yolo_xywh, self.bbox_callback)
+            self.qr_sub = rospy.Subscriber("/qr_code_data", String, self.qr_callback)
+            self.fcu_time_sub = rospy.Subscriber("/mavros/time_reference", TimeReference, self.fcu_time_callback)
+        
+        except Exception as e:
+            print("error")
+            logging.error(f"An error occurred in process_data: {str(e)}")
+        
+        
+        self.process_data()
+
+        # Initialize Publishers
+        self.server_time_pub = rospy.Publisher('/server_time', String, queue_size=10)
+        self.konum_pub = rospy.Publisher('/konum_bilgileri', KonumBilgileri, queue_size=10)
+
+        # Configure logging
+        logging.basicConfig(filename='/home/poyrazzo/catkin_ws/logs/serverlog.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+      
+    def fcu_time_callback(self, msg):
+        self.fcu_time = msg.time_ref.secs
+        self.fcu_time_nsecs = msg.time_ref.nsecs
+        
+    def qr_check(self,req):
+        self.qr_published=False
+        
+    def qr_callback(self,msg):
+        if self.qr_published is False:  # Check if the QR data has already been published
+            self.qr_data = msg.data
+            self.publish_qr()
+        else:
+            rospy.loginfo("qr zaten okundu aga")
+        
+    def lock_on_callback(self, msg):
+        self.lock_on = msg.data
+        rospy.loginfo(f"Received lock_on_status: {msg.data}")
+        self.process_data()
+    
+    def kilit_callback(self, msg):
+        self.kilit = msg.data
+        rospy.loginfo(f"Received kilit: {msg.data}")
+        self.process_data()
+
+        self.lock_on_thread = threading.Thread(target=self.send_lock_on_info)
+        self.lock_on_thread.start()
+    
+    def imu_callback(self, msg):
+        self.imu = msg
+        self.process_data()
+
+    def battery_callback(self, msg):
+        self.battery = msg
+        self.process_data()
+
+    def rel_altitude_callback(self, msg):
+        self.rel_alt = msg
+        self.process_data()
+
+    def position_callback(self, msg):
+        self.position = msg
+        self.process_data()
+
+    def speed_callback(self, msg):
+        self.speed = msg
+        self.process_data()
+
+    def state_callback(self, msg):
+        self.state = msg
+        self.process_data()
+
+    
+
+    def bbox_callback(self, msg):
+        self.bbox = msg
+        self.bbox_x = msg.x
+        self.bbox_y = msg.y
+        self.bbox_w = msg.w
+        self.bbox_h = msg.h
+        rospy.loginfo(f"Received bbox data: x={self.bbox_x}, y={self.bbox_y}, w={self.bbox_w}, h={self.bbox_h}")
+        logging.info(f"Received bbox data: x={self.bbox_x}, y={self.bbox_y}, w={self.bbox_w}, h={self.bbox_h}")
+        self.process_data()
+        
+    def login(self):
+        url = f"{self.base_url}/giris"
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        data = {
+            "kadi": self.username,
+            "sifre": self.password
+        }
+        response = self.session.post(url, json=data, headers=headers, timeout=10)
+        if response.status_code == 200:
+            print("Login successful!")
+            print(response.json())
+            self.team_number = response.json()
+        else:
+            print(f"Failed to login. Status code: {response.status_code}")
+            print("Response:", response.text)
+        return response
+
+    def process_data(self):
+        if not (self.imu and self.battery and self.rel_alt and self.position and self.speed and self.state):
+            return
+
+        if self.state.mode == 'AUTO' or self.state.mode.startswith('GUIDED'):
+            IHA_otonom = 1  # Set to 1 for autonomous modes
+        else:
+            IHA_otonom = 0  # Set to 0 for manual modes
+
+        iha_kilitlenme = 1 if self.kilit else 0
+
+        try:
+            self.get_server_time()
+
+            # Convert quaternion to euler angles
+            roll, pitch, yaw = quaternion_to_euler(self.imu.orientation.x,
+                                                   self.imu.orientation.y,
+                                                   self.imu.orientation.z,
+                                                   self.imu.orientation.w)
+
+            # Prepare data dictionary
+            data_dict = {
+                "takim_numarasi": self.team_number,
+                "IHA_enlem": self.position.latitude,
+                "IHA_boylam": self.position.longitude,
+                "IHA_irtifa": self.rel_alt.data,
+                "IHA_yonelme": yaw,
+                "IHA_dikilme": pitch,
+                "IHA_yatis": roll,
+                "IHA_hiz": calculate_speed(self.speed.twist.linear.x, self.speed.twist.linear.y, self.speed.twist.linear.z),
+                "IHA_batarya": int(self.battery.percentage * 100),
+                "IHA_otonom": IHA_otonom,
+                "iha_kilitlenme": iha_kilitlenme,
+                "hedef_merkez_X": self.bbox_x,
+                "hedef_merkez_Y": self.bbox_y,
+                "hedef_genislik": self.bbox_w,
+                "hedef_yukseklik": self.bbox_h,
+                "gps_saati": unix_to_utc_formatted(self.fcu_time, self.fcu_time_nsecs)
+            }
+            
+            logging.info("Prepared data dictionary for server update.")
+
+            # Send data to the server
+            response = self.session.post(self.server_url_telemetri_gonder, json=data_dict)
+            logging.info(f"Sent data to server: {data_dict}")
+
+            # Check server response
+            if response.status_code == 200:
+                logging.info(f"Data sent successfully: {response.json()}")
+                self.parse_and_publish_konumBilgileri(response.json())
+            else:
+                logging.error(f"Failed to send data, status code: {response.status_code}")
+
+        except Exception as e:
+            logging.error(f"An error occurred in process_data: {str(e)}")
+
+    def parse_and_publish_konumBilgileri(self, response_json):
+        try:
+            konumBilgileri_msg = KonumBilgileri()
+            # Assuming the response contains konumBilgileri field
+            konumBilgileri_data = response_json.get("konumBilgileri", [])
+            
+            for item in konumBilgileri_data:
+                konum_bilgisi = KonumBilgisi()
+                konum_bilgisi.IHA_boylam = item["IHA_boylam"]
+                konum_bilgisi.IHA_dikilme = item["IHA_dikilme"]
+                konum_bilgisi.IHA_enlem = item["IHA_enlem"]
+                konum_bilgisi.IHA_hiz = item["IHA_hiz"]
+                konum_bilgisi.IHA_irtifa = item["IHA_irtifa"]
+                konum_bilgisi.IHA_yatis = item["IHA_yatis"]
+                konum_bilgisi.IHA_yonelme = item["IHA_yonelme"]
+                konum_bilgisi.IHA_zamanfarki = item["IHA_zamanfarki"]
+                konum_bilgisi.takim_numarasi = item["takim_numarasi"]
+
+                konumBilgileri_msg.konumBilgileri.append(konum_bilgisi)
+
+            self.konum_pub.publish(konumBilgileri_msg)
+            logging.info("Published konumBilgileri message.")
+
+        except Exception as e:
+            logging.error(f"An error occurred while parsing and publishing konumBilgileri: {str(e)}")
+
+    def get_current_time(self):
+        now = datetime.now()
+        return now.hour, now.minute, now.second, now.microsecond
+        
+    def send_lock_on_info(self):
+        while not rospy.is_shutdown():
+            if self.kilit is None and self.kilit_prev is None:
+                pass
+
+            elif self.kilit_prev is None and self.kilit is not None:
+                self.kilit_prev = self.kilit
+
+            elif self.kilit_prev is True and self.kilit is True or self.kilit_prev is False and self.kilit is True:
+                self.start_time = self.get_current_time()
+
+            elif self.kilit_prev is True and self.kilit is False and self.lock_on is True:
+                self.end_time = self.get_current_time()
+
+                if self.lock_on:
+                    try:
+                        data_dict = {
+                            "kilitlenmeBaslangicZamani": {
+                                "hour": self.start_time[0],
+                                "minute": self.start_time[1],
+                                "second": self.start_time[2],
+                                "millisecond": self.start_time[3]
+                            },
+                            "kilitlenmeBitisZamani": {
+                                "hour": self.end_time[0],
+                                "minute": self.end_time[1],
+                                "second": self.end_time[2],
+                                "millisecond": self.end_time[3]
+                            },
+                            "otonom_kilitlenme": 1
+                        }
+  
+                        print("Lock-on Info Dictionary:")
+                        print(json.dumps(data_dict, indent=4))
+
+                        response = self.session.post(self.server_url_kilitlenme_bilgisi, json=data_dict)
+                        if response.status_code == 200:
+                            logging.info(f"Lock-on data sent successfully: {response.json()}")
+                        else:
+                            logging.error(f"Failed to send lock-on data, status code: {response.status_code}")
+
+                    except Exception as e:
+                        logging.error(f"An error occurred while sending lock-on data: {str(e)}")
+
+            self.kilit_prev = self.kilit  # Update previous status
+            time.sleep(0.1),
+            
+    def publish_qr(self):
+            while not rospy.is_shutdown():
+                start_time = rospy.get_param('/kamikazeBaslangicZamani', [0, 0, 0, 0])
+                end_time = rospy.get_param('/kamikazeBitisZamani', [0, 0, 0, 0])
+                if end_time != [0, 0, 0, 0]:
+                    break
+                
+            if self.qr_data and end_time != [0, 0, 0, 0]:
+                    try:
+                        start_time = rospy.get_param('/kamikazeBaslangicZamani', [0, 0, 0, 0])
+                        end_time = rospy.get_param('/kamikazeBitisZamani', [0, 0, 0, 0])
+                        
+                        rospy.loginfo(f"Start Time Retrieved: {start_time}")
+                        rospy.loginfo(f"End Time Retrieved: {end_time}")
+                        
+                        data_dict = {
+                            "kamikazeBaslangicZamani": {
+                                "hour": start_time[0],
+                                "minute": start_time[1],
+                                "second": start_time[2],
+                                "millisecond": start_time[3]
+                            },
+                            "kamikazeBitisZamani": {
+                                "hour": end_time[0],
+                                "minute": end_time[1],
+                                "second": end_time[2],
+                                "millisecond": end_time[3]
+                            },
+                            "qrMetni ": self.qr_data if self.qr_data else "No QR Data" 
+                        }
+                        
+                        print("Correction Timing Dictionary:")
+                        print(json.dumps(data_dict, indent=4))
+                        # Here you could send the data, save it to a file, etc.
+                        
+                        response = self.session.post(self.server_url_qr_bilgisi, json=data_dict)
+                        if response.status_code == 200:
+                            logging.info(f"Lock-on data sent successfully: {response.json()}")
+                        else:
+                            logging.error(f"Failed to send lock-on data, status code: {response.status_code}")
+                            
+                        self.qr_published = True
+                    except KeyError:
+                        rospy.logerr("Failed to retrieve start or end time from ROS parameters.")
+            else:
+                rospy.loginfo("no qr_data available")
+
+
+    def get_server_time(self):
+        try:
+            response = self.session.get(self.server_url_sunucusaati, timeout=10)
+            if response.status_code == 200:
+                server_time = response.json()
+                logging.info(f"Server time retrieved successfully: {server_time}")
+                time_str = f"{server_time['gun']}:{server_time['saat']}:{server_time['dakika']}:{server_time['saniye']}:{server_time['milisaniye']}"
+                self.server_time_pub.publish(time_str)
+                print(time_str)
+                return time_str
+            else:
+                logging.error(f"Failed to retrieve server time, status code: {response.status_code}")
+                return None
+        except Exception as e:
+            logging.error(f"An error occurred while retrieving server time: {str(e)}")
+            return None
+
+
+if __name__ == '__main__':
+    rospy.init_node('comm_node', anonymous=True)
+    comm_node = Comm_Node()
+    comm_node.login()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        logging.info("ROS node interrupted.")
+    except Exception as e:
+        logging.error(f"An error occurred in the main loop: {str(e)}")
