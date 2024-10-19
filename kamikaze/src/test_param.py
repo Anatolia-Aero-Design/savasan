66a4f55cd89fa6059a3431eaa99a3900e6e3b032
Poyrazzo <128072566+Poyrazzo@users.noreply.github.com>
2024-10-19 15:14:42 +0300

    Delete kamikaze directory

--- kamikaze/src/test_param.py
+++
@@ -1,9 +0,0 @@
-from utilities import set_params_for_dive, set_params_for_safe_fly,print_param
-import time
-
-if __name__ == '__main__':
-    print_param()
-    set_params_for_dive()
-    print_param()
-    time.sleep(2)
-    set_params_for_safe_fly()
