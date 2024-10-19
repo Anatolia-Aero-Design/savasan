c0c342ee07abfe5eb01cdece0fc5def3cc76ae73
Yiğit <153460142+Valvar-N@users.noreply.github.com>
2024-10-19 15:02:28 +0300

    Delete savasan_general directory

--- savasan_general/launch/simulation.sh
+++
@@ -1,5 +0,0 @@
-#open SITL
-gnome-terminal --tab --title="Ardupilot 1" --command="bash -c 'cd ~; sim_vehicle.py -v ArduPlane -L ADANA -f gazebo-zephyr --console  --map --out=127.0.0.1:14550 -I0 --sysid=1; $SHELL'" 
-
-#Start sim.launch file
-gnome-terminal --tab --title="sim.launch" --command="bash -c 'cd ~;roslaunch savasan_general sim.launch; $SHELL'"
